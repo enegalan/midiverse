09a0e7fafd67b0c0629f883feb68509877249fff
Eneko Galan <enekogalanelorza@gmail.com>
2024-08-05 22:52:51 +0200

    Development fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="/images/favicon.png"/>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>{{ isset($unreadNotifications) && $unreadNotifications > 0 ? "($unreadNotifications) " : '' }}{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
---
+++ start_development.sh
@@ -0,0 +1,2 @@
+cd /var/www/html
+npm run docker &
